891d755e1b00c5bf6c5bd7c9ffb720f667115410
Ibrahim <admin@mail.com>
2020-05-03 16:54:55 +0600

    Order status validation updated

--- dashboard/order_details.php
+++ dashboard/order_details.php
@@ -17,7 +17,9 @@
         $i = 1;
         while($row = mysqli_fetch_assoc($result)){
             $order_id = $row['order_id'];
-            $product_ids = $row['product_ids'];
+            $pid = $row['product_ids'];
+            $oq = $row['order_quantity'];
+            $op = $row['order_unit_price'];
             $amount = $row['amount'];
             $order_status = $row['order_status'];
             $date = $row['date'];
@@ -33,8 +35,53 @@
 	<div class="content">
             <div class="container-fluid table-responsive">
                 <div class="panel panel-primary">
-                    <div class="panel-heading">Order Id: <?php echo date('Hms',time()) ?></div>
-                    <div class="panel-body">Panel Content</div>
+                    <div class="panel-heading">
+                        <h3>Order Id: <?php echo $token_no; ?></h3>
+                        <?php 
+                            if ($order_status == 1) {
+                                echo "Order status "."<a href='' readonly class='btn btn-danger' title='Click to Change Status'>Pending</a>";
+                            }elseif ($order_status == 2) {
+                                echo "Order status "."<a href='' readonly class='btn btn-success' title='Click to Change Status'>Delevered</a>";
+                            }
+
+                        ?>
+                    </div>
+                    <div class="panel-body">
+                        <div class="panel-body-content">
+                            <table class="table table-responsive">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Item</th> 
+                                    <th>Quantity</th> 
+                                    <th>Price</th>  
+                                </tr>
+                            <?php 
+                                
+                                $product_ids = explode(',',$pid);
+                                $order_quantitys= explode(',',$oq);
+                                $order_unit_price= explode(',',$op);
+                                $x = 1;
+
+                                for($i = 0; $i < count($product_ids); $i++){
+                                    $query = "SELECT * FROM tbl_products WHERE product_id = '$product_ids[$i]'";
+                                    $result = mysqli_query($con,$query);
+                                    $fetch = mysqli_fetch_assoc($result);
+
+                            ?>
+                                <tr>
+                                    <td><?php echo $x++; ?></td>
+                                    <td><?php echo $fetch['product_name']; ?></td>
+                                    <td><?php echo $order_quantitys[$i]; ?></td>
+                                    <td><?php echo $order_unit_price[$i]; ?></td>
+                                </tr>
+                            <?php }?>
+                                <tr>
+                                    <td colspan="3" style="text-align: right;"><strong>Grand Total:</strong></td>
+                                    <td><?php echo $amount; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- dashboard/order_status_change.php
+++ dashboard/order_status_change.php
@@ -3,17 +3,27 @@
 
 	include 'db/db.php';
 
-	
-	if (isset($_GET['id']) && isset($_GET['action'])) {
-		$get_order_id = $_GET['id'];
-		$get_order_action = $_GET['action'];
-		$query = "UPDATE tbl_order SET order_status = '$get_order_action' WHERE order_id = '$get_order_id'";
-		$result = mysqli_query($con,$query);
+	session_start();
+	$user_id = $_SESSION['id'];
+	$query1 = "SELECT * FROM tbl_user WHERE id = '$user_id'";
+	$result1 = mysqli_query($con,$query1);
+	$fetch_result = mysqli_fetch_assoc($result1);
 
-	    if ($result) {
-	        echo "<script>alert('Status Updated Successfully')</script>";
-	        echo "<script>window.location.href='order.php'</script>";
-	    }
+	if($fetch_result['role'] == 1){
+	
+		if (isset($_GET['id']) && isset($_GET['action'])) {
+			$get_order_id = $_GET['id'];
+			$get_order_action = $_GET['action'];
+			$query = "UPDATE tbl_order SET order_status = '$get_order_action' WHERE order_id = '$get_order_id'";
+			$result = mysqli_query($con,$query);
+
+			if ($result) {
+				echo "<script>alert('Status Updated Successfully')</script>";
+				echo "<script>window.location.href='order.php'</script>";
+			}
+		}
+	}else{
+		header('location: order.php');
 	}
 
 	
