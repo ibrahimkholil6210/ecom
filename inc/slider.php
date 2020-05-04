d6c6d49a467082ec59cb60c911e1e293caee9688
Ibrahim <admin@mail.com>
2020-05-04 17:08:19 +0600

    Fixed slider responsive behavior

--- inc/slider.php
+++ inc/slider.php
@@ -3,11 +3,7 @@
 			<div class="row">
 				<div class="col-sm-12">
 					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
-							<li data-target="#slider-carousel" data-slide-to="1"></li>
-							<li data-target="#slider-carousel" data-slide-to="2"></li>
-						</ol>
+						
 						
 						<div class="carousel-inner">
 							<?php  
@@ -37,7 +33,7 @@
 									<a href="details.php?product_id=<?php echo $product_id; ?>" class="btn btn-default get">Details</a>
 								</div>
 								<div class="col-sm-6">
-									<img src="dashboard/uploads/<?php echo $product_image; ?>" class="girl img-responsive" style="height: 60vh; float: right; margin-right: 100px;" />
+									<img src="dashboard/uploads/<?php echo $product_image; ?>" class="girl img-responsive" style="height: 60vh;" />
 								</div>
 							</div>
 
@@ -45,10 +41,10 @@
 							
 						</div>
 						
-						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+						<a href="#slider-carousel" class="left control-carousel" data-slide="prev">
 							<i class="fa fa-angle-left"></i>
 						</a>
-						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+						<a href="#slider-carousel" class="right control-carousel" data-slide="next">
 							<i class="fa fa-angle-right"></i>
 						</a>
 					</div>
