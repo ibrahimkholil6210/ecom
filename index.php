15028c3449950a70ba53798748ef2496d457b4ff
Ibrahim <admin@mail.com>
2020-05-09 17:54:51 +0600

    fixed home page notice section

--- index.php
+++ index.php
@@ -7,7 +7,20 @@
 	<section>
 		<div class="container">
 			<div class="row">
-				
+				<div class="col-sm-12">
+					<div class="breadcrumbs marquee">
+						<ol class="breadcrumb ">
+							<li><a href="javascript:void(0)">Notice</a></li>
+							<li class="active">
+								<div class="flex-conteiner">
+									<marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();"> 
+										Get The Most Recent Updates From Our Site And Be Updated Your Self... 
+									</marquee>
+								</div>
+							</li>
+						</ol>
+					</div>
+				</div>
 				<?php include 'inc/slider.php'; ?>
 				<?php include 'inc/featured.php'; ?>
 				<?php include 'inc/newProduct.php'; ?>
